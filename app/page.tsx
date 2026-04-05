418136dced67314c95c2fa0fa418cb768ec34e33
sebas3002 <vsebas3002@gmail.com>
2026-04-04 21:52:41 -0600

    design: implement core brandand hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black-abyss p-24">
-      <h1 className="text-6xl font-bold text-blood-crimson tracking-tighter">
+      <h1 className="text-7xl font-extrabold text-blood-crimson uppercase tracking-tighter italic">
         DIOSCURO <span className="text-steel-fusion">CDMX</span>
       </h1>
-      <p className="mt-4 text-steel-fusion opacity-50 uppercase tracking-widest text-sm">
+      <p className="mt-6 text-steel-fusion opacity-60 uppercase tracking-[0.3em] text-xs font-bold">
         Streetwear & Custom Graphics // Próximamente
       </p>
     </main>
   );
+
+
 }
